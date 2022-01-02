a3f0539f186f3cc54882c2f49dae1a1839cf7f83
IL KYO OH <81012135+qdaszx@users.noreply.github.com>
2022-01-02 23:36:15 +0900

    Update Home.jsx
    - 간단한 사용 적용
    - 아직 매개변수를 이용하는 방법은 못봄

--- apollo/src/router/Home.jsx
+++ apollo/src/router/Home.jsx
@@ -1,7 +1,33 @@
+import { gql, useQuery } from "@apollo/client";
+
+const GET_MOVIES = gql`
+  query {
+    movies {
+      id
+      title
+      rating
+      medium_cover_image
+    }
+  }
+`;
+
 const Home = () => {
+  const { loading, error, data } = useQuery(GET_MOVIES);
+  console.log(loading, error, data);
   return (
     <>
-      <h1>Home</h1>
+      {loading ? (
+        <h1>loading...</h1>
+      ) : data ? (
+        data.movies.map((item) => {
+          return (
+            <>
+              <h1>{item.title}</h1>
+              <h2>{item.rating}</h2>
+            </>
+          );
+        })
+      ) : null}
     </>
   );
 };
