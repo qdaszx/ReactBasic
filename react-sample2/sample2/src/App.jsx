9757a494e40a8e7b6ca5f209196c17ed0b74c39a
IL KYO OH <81012135+qdaszx@users.noreply.github.com>
2022-01-20 00:20:15 +0900

    Add sortedList

--- react-sample2/sample2/src/App.jsx
+++ react-sample2/sample2/src/App.jsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
 import FoodList from "./components/FoodList";
 import items from "./mock.json";
 
 const App = () => {
+  const [order, setOrder] = useState("createdAt");
+  const sortedItems = items.sort((a, b) => b[order] - a[order]);
+
+  const handleNewestClick = () => {
+    setOrder("createdAt");
+  };
+  const handleCalorieClick = () => {
+    setOrder("calorie");
+  };
+
   return (
     <>
-      <FoodList items={items} />
+      <div>
+        <button onClick={handleNewestClick}>최신순</button>
+        <button onClick={handleCalorieClick}>칼로리순</button>
+      </div>
+      <FoodList items={sortedItems} />
     </>
   );
 };
