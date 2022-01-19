3898cd3dd0b4be22434d749ab9fb04510c339433
IL KYO OH <81012135+qdaszx@users.noreply.github.com>
2022-01-20 00:12:55 +0900

    Add sortedList
    -

--- react-sample1/sample1/src/App.jsx
+++ react-sample1/sample1/src/App.jsx
@@ -1,10 +1,21 @@
 import ReviewList from "./components/ReviewList";
 import items from "../src/mock.json";
+import { useState } from "react";
 
 const App = () => {
+  const [order, setOrder] = useState("createdAt");
+  const sortedItems = items.sort((a, b) => b[order] - a[order]);
+
+  const handleNewestClick = () => setOrder("createdAt");
+  const handleBestClick = () => setOrder("rating");
+
   return (
     <div>
-      <ReviewList items={items} />
+      <div>
+        <button onClick={handleNewestClick}>최신순</button>
+        <button onClick={handleBestClick}>베스트순</button>
+      </div>
+      <ReviewList items={sortedItems} />
     </div>
   );
 };
