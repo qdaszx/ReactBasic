158f57437817e2ca08ada07041e480400ff2a8e3
IL KYO OH <81012135+qdaszx@users.noreply.github.com>
2022-01-21 00:05:09 +0900

    Add sort func
    - 역순으로 작용할 수 있게 direction이라는 값을 1, -1로 설정하면서 반대로 정렬할 수 있게 함

--- react-sample1/sample1/src/App.jsx
+++ react-sample1/sample1/src/App.jsx
@@ -5,10 +5,25 @@ import { useState } from "react";
 const App = () => {
   const [items, setItems] = useState(mockItems);
   const [order, setOrder] = useState("createdAt");
-  const sortedItems = items.sort((a, b) => b[order] - a[order]);
+  const [direction, setDirection] = useState(1);
+  const sortedItems = items.sort((a, b) => direction * (b[order] - a[order]));
 
-  const handleNewestClick = () => setOrder("createdAt");
-  const handleBestClick = () => setOrder("rating");
+  const handleNewestClick = () => {
+    setDirection(1);
+    setOrder("createdAt");
+  };
+  const handleOldestClick = () => {
+    setDirection(-1);
+    setOrder("createdAt");
+  };
+  const handleBestClick = () => {
+    setDirection(1);
+    setOrder("rating");
+  };
+  const handleWorstClick = () => {
+    setDirection(-1);
+    setOrder("rating");
+  };
   const handleDelete = (id) => {
     const nextItems = items.filter((item) => item.id !== id);
     setItems(nextItems);
@@ -18,7 +33,9 @@ const App = () => {
     <div>
       <div>
         <button onClick={handleNewestClick}>최신순</button>
+        <button onClick={handleOldestClick}>오래된순</button>
         <button onClick={handleBestClick}>베스트순</button>
+        <button onClick={handleWorstClick}>웨스트순</button>
       </div>
       <ReviewList items={sortedItems} onDelete={handleDelete} />
     </div>
