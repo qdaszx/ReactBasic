8b345b032930211ce4030ece67a81358db1c527c
IL KYO OH <81012135+qdaszx@users.noreply.github.com>
2022-01-21 00:05:27 +0900

    Add sort func
    - sample2

--- react-sample2/sample2/src/App.jsx
+++ react-sample2/sample2/src/App.jsx
@@ -5,12 +5,24 @@ import mockItems from "./mock.json";
 const App = () => {
   const [items, setItems] = useState(mockItems);
   const [order, setOrder] = useState("createdAt");
-  const sortedItems = items.sort((a, b) => b[order] - a[order]);
+  const [direction, setDirection] = useState(1);
+
+  const sortedItems = items.sort((a, b) => direction * (b[order] - a[order]));
 
   const handleNewestClick = () => {
+    setDirection(1);
     setOrder("createdAt");
   };
-  const handleCalorieClick = () => {
+  const handleOldestClick = () => {
+    setDirection(-1);
+    setOrder("createdAt");
+  };
+  const handleHighCalorieClick = () => {
+    setDirection(1);
+    setOrder("calorie");
+  };
+  const handleLowCalorieClick = () => {
+    setDirection(-1);
     setOrder("calorie");
   };
 
@@ -23,7 +35,9 @@ const App = () => {
     <>
       <div>
         <button onClick={handleNewestClick}>최신순</button>
-        <button onClick={handleCalorieClick}>칼로리순</button>
+        <button onClick={handleOldestClick}>오래된순</button>
+        <button onClick={handleHighCalorieClick}>높은 칼로리순</button>
+        <button onClick={handleLowCalorieClick}>낮은 칼로리순</button>
       </div>
       <FoodList items={sortedItems} onDelete={handleDeleteClick} />
     </>
