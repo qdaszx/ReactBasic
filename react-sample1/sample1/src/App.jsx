6d6fc1610c28c2c13f13be99cae649ba1ec9a0d6
IL KYO OH <qdaszx@naver.com>
2022-01-23 23:18:15 +0900

    Add: network loading, error
    - 네트워크 로딩 처리 및 에러 처리

--- react-sample1/sample1/src/App.jsx
+++ react-sample1/sample1/src/App.jsx
@@ -11,6 +11,9 @@ const App = () => {
   const [offset, setOffset] = useState(0);
   const [hasNext, setHasNext] = useState(false);
   const [direction, setDirection] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [loadingError, setLoadingError] = useState(null);
+
   const sortedItems = items.sort((a, b) => direction * (b[order] - a[order]));
 
   const handleNewestClick = () => {
@@ -35,7 +38,18 @@ const App = () => {
   };
 
   const handleLoad = async (options) => {
-    const { reviews, paging } = await getReviews(options);
+    let result;
+    try {
+      setIsLoading(true);
+      setLoadingError(null);
+      result = await getReviews(options);
+    } catch (error) {
+      setLoadingError(error);
+      return;
+    } finally {
+      setIsLoading(false);
+    }
+    const { reviews, paging } = result;
     if (options.offset === 0) {
       setItems(reviews);
     } else {
@@ -62,7 +76,12 @@ const App = () => {
         <button onClick={handleWorstClick}>웨스트순</button>
       </div>
       <ReviewList items={sortedItems} onDelete={handleDelete} />
-      {hasNext && <button onClick={handleLoadMore}>더 보기</button>}
+      {hasNext && (
+        <button disabled={isLoading} onClick={handleLoadMore}>
+          더 보기
+        </button>
+      )}
+      {loadingError?.message && <span>{loadingError.message}</span>}
     </div>
   );
 };
