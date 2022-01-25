42fe8d7fbffd41e8be00c32a64b35d9d8d129416
IL KYO OH <qdaszx@naver.com>
2022-01-25 20:09:42 +0900

    direct dispatch

--- context/context-sample/src/App.jsx
+++ context/context-sample/src/App.jsx
@@ -62,38 +62,15 @@ function reducer(state, action) {
 export const UserDispatch = createContext(null);
 
 function App() {
-  const [{ username, email }, onChange, reset] = useInputs({
-    username: "",
-    email: "",
-  });
   const [state, dispatch] = useReducer(reducer, initialState);
-  const nextId = useRef(4);
 
   const { users } = state;
-
-  const onCreate = useCallback(() => {
-    dispatch({
-      type: "CREATE_USER",
-      user: {
-        id: nextId.current,
-        username,
-        email,
-      },
-    });
-    reset();
-    nextId.current += 1;
-  }, [username, email, reset]);
 
   const count = useMemo(() => countActiveUsers(users), [users]);
 
   return (
     <UserDispatch.Provider value={dispatch}>
-      <CreateUser
-        username={username}
-        email={email}
-        onChange={onChange}
-        onCreate={onCreate}
-      />
+      <CreateUser />
       <UserList users={users} />
       <div>활성 사용자 수: {count}</div>
     </UserDispatch.Provider>
