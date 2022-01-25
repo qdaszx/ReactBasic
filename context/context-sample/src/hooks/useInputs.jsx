3e2c48146a3bea8af8c2f43f101c2d330d773405
IL KYO OH <qdaszx@naver.com>
2022-01-25 19:13:02 +0900

    update useInputs hook
    useState 대신 useReducer를 이용

--- context/context-sample/src/hooks/useInputs.jsx
+++ context/context-sample/src/hooks/useInputs.jsx
@@ -1,14 +1,35 @@
-import { useState, useCallback } from "react";
+import { useCallback, useReducer } from "react";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "CHANGE_INPUT":
+      return {
+        ...state,
+        [action.name]: action.value,
+      };
+    case "RESET_INPUT":
+      return Object.keys(state).reduce((acc, current) => {
+        acc[current] = "";
+        return acc;
+      }, {});
+    default:
+      return state;
+  }
+};
 
 const useInputs = (initialForm) => {
-  const [form, setForm] = useState(initialForm);
+  const [form, dispatch] = useReducer(reducer, initialForm);
 
   const onChange = useCallback((e) => {
     const { name, value } = e.target;
-    setForm((form) => ({ ...form, [name]: value }));
+    dispatch({
+      type: "CHANGE_INPUT",
+      name,
+      value,
+    });
   }, []);
 
-  const reset = useCallback(() => setForm(initialForm), [initialForm]);
+  const reset = useCallback(() => dispatch({ type: "RESET_INPUT" }), []);
 
   return [form, onChange, reset];
 };
