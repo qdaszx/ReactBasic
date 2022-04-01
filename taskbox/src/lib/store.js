f0d6f6df6199a7d3faf9ba71fc1512eff7dd4e56
IL KYO OH <qdaszx@naver.com>
2022-04-02 08:00:00 +0900

    interactive stories

--- taskbox/src/lib/store.js
+++ taskbox/src/lib/store.js
@@ -18,14 +18,12 @@ export const fetchTasks = createAsyncThunk('todos/fetchTodos', async () => {
     'https://jsonplaceholder.typicode.com/todos?userId=1'
   )
   const data = await response.json();
-  const result = data.map((task) => {
-    const { id, title, completed } = task
-    return {
-      id: `${id}`,
-      title: title,
-      state: completed ? 'TASK_ARCHIVED' : 'TASK_INBOX'
-    }
-  })
+  const result = data.map((task) => ({
+    id: `${task.id}`,
+    title: task.title,
+    state: task.completed ? 'TASK_ARCHIVED' : 'TASK_INBOX'
+  }
+  ))
   return result;
 })
 
